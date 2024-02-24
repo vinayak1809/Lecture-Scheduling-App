c2cb1aba93e86d806ac64eaf37a03a3361c634dd
vinayak1809 <vinayakrshinde18@gmail.com>
2024-02-25 01:38:45 +0530

    change in routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,56 +4,40 @@ import Instructor from "./pages/Instructor/Instructor";
 import AdminPanel from "./pages/Admin/AdminPanel";
 import AddCourse from "./Components/Add-Course/AddCourse";
 import ScheduleLecture from "./Components/Schedule-Lecture/ScheduleLecture";
-import { useEffect, useState } from "react";
 import Logout from "./Components/Logout/Logout";
-import axios from "axios";
-import { url } from "./utils/url";
+import NotFound from "./pages/Not-Found/NotFound";
 
-const token = localStorage.getItem("token");
+import { useContext } from "react";
+import UserContext from "./context/UserContext";
 
 function App() {
-  const [user, setUser] = useState({ role: "" });
-
-  async function fetchData() {
-    try {
-      const response = await axios.get(`${url}/login-details`, {
-        headers: {
-          Authorization: "Bearer " + token,
-        },
-      });
-      setUser({ role: response.data.user.role });
-    } catch (error) {
-      console.error("something went wrong");
-    }
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
+  const { user } = useContext(UserContext);
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          {user.role == "admin" ? (
+          {user.role === "admin" ? (
             <>
-              p
+              <Route path="*" element={<NotFound />} />
               <Route path="/" element={<Navigate to="/admin" />} />
+              <Route path="/login" element={<Navigate to="/admin" />} />
               <Route path="/admin" element={<AdminPanel />} />
               <Route path="/add-course" element={<AddCourse />} />
               <Route path="/schedule-lecture" element={<ScheduleLecture />} />
               <Route path="/logout" element={<Logout />} />
             </>
-          ) : user.role == "instructor" ? (
+          ) : user.role === "instructor" ? (
             <>
+              <Route path="*" element={<NotFound />} />
               <Route path="/" element={<Navigate to="/instructor" />} />
+              <Route path="/login" element={<Navigate to="/instructor" />} />
               <Route path="/instructor" element={<Instructor />} />
               <Route path="/logout" element={<Logout />} />
             </>
           ) : (
             <>
+              <Route path="*" element={<NotFound />} />
               <Route path="/" element={<Navigate to="/login" />} />
-
               <Route path="/login" element={<Login />} />
             </>
           )}
